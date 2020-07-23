e0ea1c6eb1fa2c6a1dd8a0fd4ec27d82bf1e51d2
saul alejos garay <43302778+saulwolfdev@users.noreply.github.com>
2020-07-23 20:40:18 -0300

    creando el entorno backend, variables de entorno

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,12 @@
-console.log("hola");
+const express=require("express");
+const cors=require("cors");
+const app=express();
+const PORT=process.env.PORT||8000;
+app.use(cors());
+app.use(express.json());
+app.get("/",(req, res)=>{
+	res.send("hello man");
+})
+app.listen(PORT,()=>{
+	console.log(`Listening on => ${PORT}`);
+})
