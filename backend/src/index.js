19cb5e782dcdc713f2bedeed0c3f92347845bf94
saul alejos garay <43302778+saulwolfdev@users.noreply.github.com>
2020-07-23 22:12:30 -0300

    connect database ok

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,27 @@
 const express=require("express");
+const mongoose=require("mongoose")
 const cors=require("cors");
 const app=express();
-const PORT=process.env.PORT||8000;
+const PORT=process.env.PORT||3000;
+
+if(process.env.NODE_ENV!="production"){
+	require("dotenv").config();
+}
 app.use(cors());
 app.use(express.json());
+
 app.get("/",(req, res)=>{
 	res.send("hello man");
-})
+});
+try{
+	mongoose.connect(process.env.MONGO_DB_CONNECTION,{
+		useNewUrlParser:true,
+		useUnifiedTopology:true
+	})
+	console.log("MongoDB CONNECT OK")
+}catch(error){
+	console.log(error)
+}
 app.listen(PORT,()=>{
 	console.log(`Listening on => ${PORT}`);
 })
